c5a0d11264bbe4249dd065c5bacf04a9d252f0f7
Philip lee <philiplee1999@gmail.com>
2023-10-20 17:13:28 -0700

    more tricks

--- Learning/Learning.cpp
+++ Learning/Learning.cpp
@@ -43,4 +43,32 @@ void cleverMinNum()
 	// x1 will hold the minimum value among a, b, c, d, e
 }
 
+// --------------------------
+// Tip 3: Finding the Size of an Array
+// --------------------------
+
+// Macro Approach: Using a Macro to Get Array Size
+// This example demonstrates how to find the size of an array using
+// a macro. The macro divides the total size of the array in bytes
+// by the size of a single element to get the number of elements.
+#define ARRAYSIZEM(b) (sizeof(b) / sizeof(b[0]))
+
+void getArrSize()
+{
+	int a[] = { 1, 2, 3, 4, 5, 6 };
+	std::cout << ARRAYSIZEM(a) << std::endl;
+}
+
+// Template Approach: Using a Template Function to Get Array Size
+// This example uses a template function to find the size of an array.
+// The template argument deduction mechanism of C++ automatically deduces
+// the type and size of the array, making this approach type-safe and
+// more idiomatic in modern C++.
+template<typename T, size_t N>
+size_t ARRAYSIZET(T(&a)[N]) // Note: T(&a)[N] is a reference to an array of N elements of type T
+							// This is important because we stop the array from decaying into a
+							// pointer when passed into a function.
+{
+	return N;
+}
 
